715139da4a51603a0f101c49c9c460dddda43c5b
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2024-03-06 18:25:57 +0100

    Add related source and header files

---
+++ test/src/test_base.hpp
@@ -0,0 +1,70 @@
+/**
+ *    @file  test_base.hpp
+ *   @brief  Test base header file.
+ *
+ *  This header file includes essential macros such as test data directory which can be
+ *  redefine during compile time and definitions here are just default values.
+ *
+ *  @author  Ali Ghaffaari (\@cartoonist), <ali.ghaffaari@uni-bielefeld.de>
+ *
+ *  @internal
+ *       Created:  Wed Mar 06, 2024  18:56
+ *  Organization:  Universität Bielefeld
+ *     Copyright:  Copyright (c) 2024, Ali Ghaffaari
+ *
+ *  This source code is released under the terms of the MIT License.
+ *  See LICENSE file for more information.
+ */
+
+#ifndef DIVERG_TEST_BASE_HPP__
+#define DIVERG_TEST_BASE_HPP__
+
+#include <string>
+#include <random>
+
+#include "catch2/catch.hpp"
+
+#include "test_config.hpp"
+
+#define TEMPLATE_SCENARIO TEMPLATE_TEST_CASE
+#define TEMPLATE_SCENARIO_SIG TEMPLATE_TEST_CASE_SIG
+
+#ifndef TEST_DATA_DIR
+#define TEST_DATA_DIR PROJECT_SOURCE_DIR "/test/data"
+#endif
+
+namespace rnd {
+  inline std::random_device&
+  get_rd()
+  {
+    thread_local static std::random_device rd;
+    return rd;
+  }
+
+  inline unsigned int&
+  get_iseed()
+  {
+    thread_local static unsigned int iseed = get_rd()();
+    return iseed;
+  }
+
+  inline std::mt19937&
+  get_rgn()
+  {
+    thread_local static std::mt19937 rgn( get_iseed() );
+    return rgn;
+  }
+
+  inline void
+  set_seed( unsigned int seed )
+  {
+    if ( seed != 0 ) {
+      get_iseed() = seed;
+      get_rgn().seed( seed );
+    }
+  }
+}  /* ---  end of namespace rnd  --- */
+
+static const std::string test_data_dir( TEST_DATA_DIR );
+
+#endif  /* --- #ifndef DIVERG_TEST_BASE_HPP__ --- */
---
+++ test/src/test_main.cpp
@@ -0,0 +1,57 @@
+/**
+ *    @file  test_main.cpp
+ *   @brief  Unit test framework (Catch2) implementation file.
+ *
+ *  In order to compile the unit testing framework once, not for each tranlation unit
+ *  which includes the header file, we put the `CATCH_CONFIG_MAIN` macro in a separated
+ *  source file while the header file is included. This file should be linked with other
+ *  test implementations.
+ *
+ *  @author  Ali Ghaffaari (\@cartoonist), <ali.ghaffaari@uni-bielefeld.de>
+ *
+ *  @internal
+ *       Created:  Wed Mar 06, 2024  18:56
+ *  Organization:  Universität Bielefeld
+ *     Copyright:  Copyright (c) 2024, Ali Ghaffaari
+ *
+ *  This source code is released under the terms of the MIT License.
+ *  See LICENSE file for more information.
+ */
+
+#include<Kokkos_Core.hpp>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+#include "test_base.hpp"
+
+
+class MyTestEventListener : public Catch::TestEventListenerBase {
+public:
+    using Catch::TestEventListenerBase::TestEventListenerBase;
+
+    void set_rnd_seed() {
+      auto seed = Catch::rngSeed();
+      if ( seed != 0 ) {
+        std::cout << "Setting random generator seed to " << seed << "..." << std::endl;
+        rnd::set_seed( seed );
+      }
+    }
+
+    void testRunStarting( Catch::TestRunInfo const& ) override {
+      this->set_rnd_seed();
+      /*
+      // For debugging
+      Kokkos::InitializationSettings args;
+      args.set_num_threads( 1 );
+      Kokkos::initialize( args );
+      */
+      Kokkos::initialize();
+    }
+
+    void testRunEnded( Catch::TestRunStats const& testRunStats ) override {
+      Kokkos::finalize();
+    }
+};
+
+CATCH_REGISTER_LISTENER(MyTestEventListener)
