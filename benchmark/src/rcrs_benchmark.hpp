e23a9c7d9a0ea232f6502cef6183aeacd3478db9
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2024-05-17 18:48:25 +0200

    [rcrs_benchmark refactor] Move Options to header

--- benchmark/src/rcrs_benchmark.hpp
+++ benchmark/src/rcrs_benchmark.hpp
@@ -29,6 +29,237 @@
   std::to_string( RCRS_BENCHMARK_DEFAULT_TEAM_WORK_SIZE )
 
 
+template< typename TOrdinal = int32_t, typename TSize = uint64_t >
+struct Options {
+  /* === TYPE MEMBERS === */
+  using ordinal_type = TOrdinal;
+  using size_type = TSize;
+  /* === DATA MEMBERS === */
+  std::string graph_path;
+  std::string format;
+  TOrdinal n;
+  TSize nnz;
+  unsigned int seed;
+  unsigned int dlo;
+  unsigned int dup;
+  std::string seg_name;
+  std::string reg_name;
+  std::string partition;
+  std::string grid;
+  unsigned int l1size;
+  bool run_kokkos;
+  bool run_rspgemm;
+  bool host;
+  bool compare;
+  int verbose;
+  /* === LIFECYCLE === */
+  Options()
+      : graph_path( "" ), format( "" ), n( 0 ), nnz( 0 ), dlo( 0 ), dup( 0 ),
+        seg_name( "" ), reg_name( "" ), partition( "team-sequential" ),
+        grid( "auto" ), l1size( 8192 ), run_kokkos( true ),
+        run_rspgemm( true ), host( false ), compare( false ), verbose( 0 )
+  { }
+};
+
+template< typename TOrdinal = Options<>::ordinal_type,
+          typename TSize = Options<>::size_type >
+Options< TOrdinal, TSize >
+parse_arguments( int argc, char* argv[] )
+{
+  Options< TOrdinal, TSize > opts;
+
+  // Parse command line arguments
+  for ( int i = 1; i < argc; i++ ) {
+    if ( ( strcmp( argv[ i ], "-g" ) == 0 ) || ( strcmp( argv[ i ], "--graph" ) == 0 ) ) {
+      opts.graph_path = argv[ ++i ];
+      std::cout << "Parameter: graph <- " << opts.graph_path << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-f" ) == 0 ) || ( strcmp( argv[ i ], "--format" ) == 0 ) ) {
+      opts.format = argv[ ++i ];
+      std::cout << "Parameter: format <- " << opts.format << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-n" ) == 0 ) || ( strcmp( argv[ i ], "--order" ) == 0 ) ) {
+      opts.n = pow( 2, atoi( argv[ ++i ] ) );
+      std::cout << "Parameter: n <- " << opts.n << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-z" ) == 0 ) || ( strcmp( argv[ i ], "--nnz" ) == 0 ) ) {
+      opts.nnz = pow( 2, atoi( argv[ ++i ] ) );
+      std::cout << "Parameter: nnz <- " << opts.nnz << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-S" ) == 0 ) || ( strcmp( argv[ i ], "--seed" ) == 0 ) ) {
+      opts.seed = atoi( argv[ ++i ] );
+      std::cout << "Parameter: seed <- " << opts.seed << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-d" ) == 0 ) || ( strcmp( argv[ i ], "--dist-lower" ) == 0 ) ) {
+      opts.dlo = atoi( argv[ ++i ] );
+      std::cout << "Parameter: d <- " << opts.dlo << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-D" ) == 0 ) || ( strcmp( argv[ i ], "--dist-upper" ) == 0 ) ) {
+      opts.dup = atoi( argv[ ++i ] );
+      std::cout << "Parameter: D <- " << opts.dup << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-K" ) == 0 ) || ( strcmp( argv[ i ], "--disable-kokkos" ) == 0 ) ) {
+      opts.run_kokkos = false;
+      std::cout << "Parameter: disable-kokkos <- "
+                << ( opts.run_kokkos ? "false" : "true" ) << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-R" ) == 0 ) || ( strcmp( argv[ i ], "--disable-rspgemm" ) == 0 ) ) {
+      opts.run_rspgemm = false;
+      std::cout << "Parameter: disable-rspgemm <- "
+                << ( opts.run_rspgemm ? "false" : "true" ) << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-s" ) == 0 ) || ( strcmp( argv[ i ], "--segment-name" ) == 0 ) ) {
+      opts.seg_name = argv[ ++i ];
+      std::cout << "Parameter: segment-name <- " << opts.seg_name << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-r" ) == 0 ) || ( strcmp( argv[ i ], "--region-name" ) == 0 ) ) {
+      opts.reg_name = argv[ ++i ];
+      std::cout << "Parameter: region-name <- " << opts.reg_name << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-p" ) == 0 ) || ( strcmp( argv[ i ], "--partition" ) == 0 ) ) {
+      opts.partition = argv[ ++i ];
+      std::cout << "Parameter: partition <- " << opts.partition << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-G" ) == 0 ) || ( strcmp( argv[ i ], "--grid" ) == 0 ) ) {
+      opts.grid = argv[ ++i ];
+      std::cout << "Parameter: grid <- " << opts.grid << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-l" ) == 0 ) || ( strcmp( argv[ i ], "--l1-size" ) == 0 ) ) {
+      opts.l1size = pow( 2, atoi( argv[ ++i ] ) );
+      std::cout << "Parameter: l1size <- " << opts.l1size << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-H" ) == 0 ) || ( strcmp( argv[ i ], "--host" ) == 0 ) ) {
+      opts.host = true;
+      std::cout << "Parameter: host <- "
+                << ( opts.host ? "true" : "false" ) << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-c" ) == 0 ) || ( strcmp( argv[ i ], "--compare" ) == 0 ) ) {
+      opts.compare = true;
+      std::cout << "Parameter: compare <- "
+                << ( opts.compare ? "true" : "false" ) << std::endl;
+    }
+    else if ( ( strcmp( argv[ i ], "-v" ) == 0 ) || ( strcmp( argv[ i ], "--verbose" ) == 0 ) ) {
+      opts.verbose++;
+    }
+    else if ( ( strcmp( argv[ i ], "-h" ) == 0 ) || ( strcmp( argv[ i ], "--help" ) == 0 ) ) {
+      std::cout
+          << "Options:\n"
+          << "  * Adjacency matrix as input (overrides -n and -z)\n"
+          << "    --graph (-g) <path>:       graph file path\n"
+          << "    --format (-f) <str>:       specify graph file format when it is not\n"
+          << "                               automatically detected; e.g. when GFA has no header\n"
+          << "                               (formats: 'gfa', 'gfa1', 'gfa2')\n"
+          << "    --region-name (-r) <str>:  specify target region by path id (embedded in graph)\n"
+          << "                               (overrides -s)\n"
+          << "    --segment-name (-s) <str>: specify target region by the name of its first node\n"
+          << "                               or segment (default: first node/segment in the graph)\n"
+          << "  * Random matrix as input\n"
+          << "    --order (-n) <int>:        exponent num, determines number of rows 2^num\n"
+          << "                               (default: 2^12 = 4096 or 2^((z+1)/2) if -z given)\n"
+          << "    --nnz (-z) <int>:          exponent num, determines total matrix size 2^num\n"
+          << "                               (default: 2^22 = 4096*1024 or 2^n*1024 if -n provided)\n"
+          << "    --rng-seed (-S) <int>:     Seed for random number generation (default: random)\n"
+          << "  --dist-lower (-d) <int>:     lower bound of outer distance, i.e. fragment size\n"
+          << "                               (default: 0)\n"
+          << "  --dist-upper (-D) <int>:     upper bound of outer distance, i.e. fragment size\n"
+          << "                               (required)\n"
+          << "  --partition (-p) <str>:      Partitioning schemes: 'team-sequential' (default),\n"
+          << "                               'thread-sequential', 'thread-parallel', and\n"
+          << "                               'thread-range-paralell'\n"
+          << "  --grid (-G) <str>            Execution grid options: 'auto' (auto-tuning; default),\n"
+          << "                               'suggested' (based on row density of input)\n"
+          << "  --grid (-G) <int,int[,int]>  '<team size>,<vector size>[,<team work size>]'\n"
+          << "                               in which default team work size is "
+          << RCRS_BENCHMARK_DEFAULT_TEAM_WORK_SIZE_STR << "\n"
+          << "                               (any non-numeric, non-whitespace delimiter works)\n"
+          << "                               examples: '-G 32,4,16' or '-G 16x16'\n"
+          << "  --l1-size (-l) <int>:        exponent num, determines L1 size of accumulator 2^num;\n"
+          << "                               with constraints: 10<=num<=15 (default: 2^13 = 1KB)\n"
+          << "  --host (-H):                 run on host (default: false)\n"
+          << "  --disable-kokkos (-K):       disable benchmarking with Kokkos SpGEMM (PairG)\n"
+          << "                               (default: false)\n"
+          << "  --disable-rspgemm (-R):      disable benchmarking with rSpGEMM (DiVerG)\n"
+          << "                               (default: false)\n"
+          << "  --compare (-c):              Compare resulting indices build by DiVerG and PairG\n"
+          << "                               given that -K is not set (default: false)\n"
+          << "  --verbose (-v):              increase verbose level by one (default: 0)\n"
+          << "  --help (-h):                 print this message"
+          << std::endl;
+      exit( EXIT_SUCCESS );
+    }
+    else {
+      std::cerr << "[ERROR] Unknown argument '" << argv[ i ] << "'" << std::endl;
+      exit( EXIT_FAILURE );
+    }
+  }
+
+  if ( opts.verbose ) std::cout << "Verbose level: " << opts.verbose << std::endl;
+
+  return opts;
+}
+
+template< typename TOrdinal, typename TSize >
+void
+check_options( Options< TOrdinal, TSize >& opts )
+{
+  if ( opts.graph_path.empty() ) {
+    if ( opts.nnz == 0 ) {
+      if ( opts.n == 0 )  // If both opts.n and opts.nnz are not provided:
+        opts.nnz = pow( 2, 22 );
+      else  // If opts.n is provided but opts.nnz is not:
+        opts.nnz = opts.n * 1024;
+      std::cout << "[DEFAULT] nnz = " << opts.nnz << std::endl;
+    }
+
+    // If opts.n is not provided, set order to 2^((log2(opts.nnz)+1)/2).
+    if ( opts.n == 0 ) {
+      int power = 1;
+      auto temp = opts.nnz;
+      while ( ( temp /= 2 ) ) ++power;
+      opts.n = pow( 2, power / 2 );
+      std::cout << "[DEFAULT] n = " << opts.n << std::endl;
+    }
+
+    if ( opts.n < 0 /* || opts.nnz < 0 */ ) {
+      std::cerr << "[ERROR] nnz must be greater than zero" << std::endl;
+      exit( EXIT_FAILURE );
+    }
+
+    if ( opts.nnz / opts.n > static_cast< TSize >( opts.n ) ) {
+      std::cerr << "[ERROR] Non-zero values cannot be fit (nnz > n*n)"
+                << std::endl;
+      exit( EXIT_FAILURE );
+    }
+  }
+  else {
+    if ( !gum::util::readable( opts.graph_path ) ) {
+      std::cerr << "[ERROR] Input file is missing or not readable"
+                << std::endl;
+      exit( EXIT_FAILURE );
+    }
+  }
+
+  if ( opts.dup == 0 ) {
+    std::cerr << "[ERROR] Too few arguments (required argument: '-D')"
+              << std::endl;
+    exit( EXIT_FAILURE );
+  }
+
+  if ( opts.dup < opts.dlo ) {
+    std::cerr << "[WARN] Lower bound of distance constraints is larger than "
+                 "upper bound"
+              << std::endl;
+    std::swap( opts.dlo, opts.dup );
+    std::cerr << "[WARN] Swapped distance constraints bounds: (" << opts.dlo
+              << ", " << opts.dup << ")" << std::endl;
+  }
+
+  if ( !opts.run_kokkos && !opts.run_rspgemm ) {
+    std::cerr << "[ERROR] Nothing to do!" << std::endl;
+    exit( EXIT_FAILURE );
+  }
+}
+
 template< typename TGraph >
 TGraph
 load_graph( std::string const& graph_path, std::string const& format )
