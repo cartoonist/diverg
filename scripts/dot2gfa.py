359d0dd898bc754f11cfd7670d835e73f7e89b92
Ali Ghaffaari <ali.ghaffaari@uni-bielefeld.de>
2024-07-30 21:59:22 +0200

    [dot2gfa] Fix code style issues

--- scripts/dot2gfa.py
+++ scripts/dot2gfa.py
@@ -5,6 +5,7 @@ import bisect
 import re
 from Bio import SeqIO
 
+
 def remove_overlapping_prefix(vertexLabels, edges, k):
     """Remove overlapping prefix from selected vertices.
 
@@ -12,36 +13,33 @@ def remove_overlapping_prefix(vertexLabels, edges, k):
                remove first (k-1) characters from its label
     """
     trimVertexLabel = [0] * len(vertexLabels)
-    for (u,v) in edges:
-        assert(u >= 0)
-        assert(u < len(vertexLabels))
-        assert(v >= 0)
-        assert(v < len(vertexLabels))
+    for (u, v) in edges:
+        assert u >= 0
+        assert u < len(vertexLabels)
+        assert v >= 0
+        assert v < len(vertexLabels)
         trimVertexLabel[v] = 1
 
     for i in range(len(vertexLabels)):
         if trimVertexLabel[i] == 1:
-            vertexLabels[i] = vertexLabels[i][k-1:]
-
+            vertexLabels[i] = vertexLabels[i][k - 1:]
 
 
 def get_out_neighbours(edges):
     """Compute out-neighbours of each vertex."""
     out_index = dict()
-    for (u,v) in edges:
+    for (u, v) in edges:
         out_index[u] = list()
 
-    for (u,v) in edges:
+    for (u, v) in edges:
         out_index[u].append(v)
 
     return out_index
 
 
 @click.command()
-@click.option('-k', type=int, default=25, show_default=True,
-              help='k-mer size')
-@click.option('--fasta', '-f', type=click.Path(exists=True),
-              help='FASTA file')
+@click.option('-k', type=int, default=25, show_default=True, help='k-mer size')
+@click.option('--fasta', '-f', type=click.Path(exists=True), help='FASTA file')
 @click.option('--output', '-o', type=click.Path(), default="output.gfa",
               show_default=True, help='Output GFA file')
 @click.argument('input', type=click.Path(exists=True))
@@ -63,10 +61,11 @@ def dot2gfa(input, k, fasta, output):
             tokens = line.strip().split()
             if len(tokens) == 2:  # must be a vertex label
                 currentVertexId += 1
-                assert(int(tokens[0]) == currentVertexId)
-                # remove leading '[label="' and trailing '"]", choose the last 'position:len'
+                assert int(tokens[0]) == currentVertexId
+                # remove leading '[label="' and trailing '"]", and
+                # choose the last 'pos:len'
                 label_range = tokens[1][8:-2].split(',')[-1].split(':')
-                position = int(label_range[0])
+                pos = int(label_range[0])
                 length = int(label_range[1])
                 idx = bisect.bisect_left(lpos, position + 1) - 1
                 offset = position - lpos[idx]
@@ -74,16 +73,16 @@ def dot2gfa(input, k, fasta, output):
                 seq = re.sub(r'[N]', 'A', lrec[idx])
                 # NOTE: splitMEM add a "N" at the end of each FASTA entry
                 seq += "N"
-                subseq = seq[offset:offset+length]
+                subseq = seq[offset:offset + length]
                 subseq = re.sub(r'[^ACGTN\$]', '', subseq)  #get rid of ambiguous character
                 subseq = re.sub(r'[\$]', 'N', subseq)       # get rid of ambiguous characters
                 vertexLabels.append(subseq)
             elif len(tokens) == 3:
-                if tokens[1] == "->": # must be an edge
-                    assert(int(tokens[0]) == currentVertexId)
+                if tokens[1] == "->":  # must be an edge
+                    assert int(tokens[0]) == currentVertexId
                     edges.append((int(tokens[0]), int(tokens[2])))
 
-    #Remove duplicates
+    # Remove duplicates
     edges = list(dict.fromkeys(edges))
 
     remove_overlapping_prefix(vertexLabels, edges, k)
